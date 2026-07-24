dc6e19582b0cd0ec32276f45fc49fe6a98c1e6b6
refutom <nikita057000@gmail.com>
2026-07-24 20:25:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Токен, который дал @BotFather
-BOT_TOKEN = "СЮДА_СВОЙ_ТОКЕН"
+BOT_TOKEN = "8618366166:AAEsBB2wqZHh9c7tz22RXLgYjMr6uzUG_HI"
 
 # Имя файла, где бот хранит арты (создастся сам)
 DB_FILE = "waifu_db.json"
